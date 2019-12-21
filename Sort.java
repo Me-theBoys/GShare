069004c28efe7d4ea1ce9c9c4821a244c63130f3
ejderado <57733334+ejderado@users.noreply.github.com>
2019-12-21 23:11:36 +0300

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 
 public class Sort {
 
+    public static final char LENDING = 'L';
+    public static final char BORROWING = 'B';
+
+
     public static final int ALL = 0;
     public static final int EDUCATION = 1;
     public static final int ELECTRONIC = 2;
@@ -17,13 +21,13 @@ public class Sort {
     public static final int BOOKS = 7;
     public static final int OTHER = 8;
 
-    public static ArrayList<Notice> sortByKeyWord ( ArrayList<Notice> noticeList , String keyWord , char noticeType ) {
+    public static ArrayList<Notice> sortByKeyWord ( ArrayList<Notice> noticeList , String keyWord , char noticeType ) {//TESTED AND WORKS
         ArrayList<Notice> sortedList = new ArrayList<Notice>();
 
-        if( noticeType == 'B'){
+        if( noticeType == BORROWING){
             noticeList = getBorrowings( noticeList );
         }
-        if( noticeType == 'L'){
+        if( noticeType == LENDING){
             noticeList = getLendings( noticeList );
         }
         for ( int i = 0 ; i < noticeList.size() ; i++ ) {
@@ -35,13 +39,13 @@ public class Sort {
         return sortedList;
     }
 
-    public static ArrayList<Notice> sortByCategory ( ArrayList<Notice> noticeList , int categoryType, char noticeType ) {
+    public static ArrayList<Notice> sortByCategory ( ArrayList<Notice> noticeList , int categoryType, char noticeType ) {//TESTED AND WORKS
         ArrayList<Notice> sortedList = new ArrayList<Notice>();
 
-        if( noticeType == 'B'){
+        if( noticeType == BORROWING){
             noticeList = getBorrowings( noticeList );
         }
-        if( noticeType == 'L'){
+        if( noticeType == LENDING){
             noticeList = getLendings( noticeList );
         }
         for ( int i = 0 ; i < noticeList.size() ; i++ ) {
@@ -53,10 +57,12 @@ public class Sort {
         return sortedList;
     }
 
-    public static ArrayList<Notice> sortByGInterval ( ArrayList<Notice> noticeList , int startG , int endG  ) {
+    public static ArrayList<Notice> sortByGInterval ( ArrayList<Notice> noticeList , int startG , int endG  ) {//TESTED AND WORK
         ArrayList<Notice> sortedList = new ArrayList<Notice>();
 
-        for ( int i = 0 ; i < noticeList.size() ; i++ ) {
+        noticeList = getLendings( noticeList );
+
+        for ( int i = 0 ; i < noticeList.size(); i++ ) {
             if ( noticeList.get(i).getG() <= endG && startG <= noticeList.get(i).getG() ) {
                 sortedList.add(noticeList.get(i));
             }
@@ -65,13 +71,13 @@ public class Sort {
         return sortedList;
     }
 
-    public static ArrayList<Notice> sortByDistance ( ArrayList<Notice> noticeList , User user , int interval, char noticeType ) {
+    public static ArrayList<Notice> sortByDistance ( ArrayList<Notice> noticeList , User user , int interval, char noticeType ) {//TESTED AND PROBABLY IT WORKS HARD TO SEE
         ArrayList<Notice> sortedList = new ArrayList<Notice>();
 
-        if( noticeType == 'B'){
+        if( noticeType == BORROWING){
             noticeList = getBorrowings( noticeList );
         }
-        if( noticeType == 'L'){
+        if( noticeType == LENDING){
             noticeList = getLendings( noticeList );
         }
         for ( int i = 0 ; i < noticeList.size() ; i++ ) {
@@ -80,17 +86,17 @@ public class Sort {
             }
         }
 
-        for( int j = 0; j < noticeList.size(); j++ ) {
+        for( int j = 0; j < sortedList.size(); j++ ) {
             int smallestPos = j;
 
-            for (int i = j; i < noticeList.size(); i++) {
-                if (user.getLocation().distanceFrom(noticeList.get(i).getLocation()) < user.getLocation().distanceFrom(noticeList.get(smallestPos).getLocation()))
+            for (int i = j; i < sortedList.size(); i++) {
+                if (user.getLocation().distanceFrom(sortedList.get(i).getLocation()) < user.getLocation().distanceFrom(sortedList.get(smallestPos).getLocation()))
                     smallestPos = i;
             }
 
-            Notice temp = noticeList.get(smallestPos);
-            noticeList.set( smallestPos, noticeList.get(j) );
-            noticeList.set( j , temp );
+            Notice temp = sortedList.get(smallestPos);
+            sortedList.set( smallestPos, sortedList.get(j) );
+            sortedList.set( j , temp );
 
         }
 
@@ -98,13 +104,13 @@ public class Sort {
         return sortedList;
     }
 
-    public static ArrayList<Notice> sortByBookmark ( ArrayList<Notice> noticeList , User user, char noticeType ) {
+    public static ArrayList<Notice> sortByBookmark ( ArrayList<Notice> noticeList , User user, char noticeType ) {//TESTED AND WORKS
         ArrayList<Notice> sortedList = new ArrayList<Notice>();
 
-        if( noticeType == 'B'){
+        if( noticeType == BORROWING){
             noticeList = getBorrowings( noticeList );
         }
-        if( noticeType == 'L'){
+        if( noticeType == LENDING){
             noticeList = getLendings( noticeList );
         }
         for ( int i = 0 ; i < noticeList.size() ; i++ ) {
@@ -118,25 +124,24 @@ public class Sort {
         return sortedList;
     }
 
-    public static ArrayList<Notice> sortByLexiography ( ArrayList<Notice> noticeList, char noticeType ) {
+    public static ArrayList<Notice> sortByLexiography ( ArrayList<Notice> noticeList, char noticeType ) {//TESTED AND WORKS
 
-        if( noticeType == 'B'){
+        if( noticeType == BORROWING){
             noticeList = getBorrowings( noticeList );
         }
-        if( noticeType == 'L'){
+        if( noticeType == LENDING){
             noticeList = getLendings( noticeList );
         }
 
         for ( int i = 0 ; i < noticeList.size() ; i++ ) {
             int smallestIndex = i;
 
-            for ( int j = i ; i < noticeList.size() ; j++ ) {
+            for ( int j = i ; j < noticeList.size() ; j++ ) {
                 if ( noticeList.get(smallestIndex).getName().compareTo(noticeList.get(j).getName()) > 0 ) {
                     smallestIndex = j;
                 }
             }
 
-            System.out.println(smallestIndex);
             Notice temp = noticeList.get(smallestIndex);
             noticeList.set( smallestIndex , noticeList.get(i) );
             noticeList.set(i , temp);
@@ -144,18 +149,18 @@ public class Sort {
         return noticeList;
     }
 
-    public static ArrayList<Notice> sortByPostTime ( ArrayList<Notice> noticeList, char noticeType ){
+    public static ArrayList<Notice> sortByPostTime ( ArrayList<Notice> noticeList, char noticeType ){//TESTED AND WORKS
 
-        if( noticeType == 'B'){
+        if( noticeType == BORROWING){
             noticeList = getBorrowings( noticeList );
         }
-        if( noticeType == 'L'){
+        if( noticeType == LENDING){
             noticeList = getLendings( noticeList );
         }
         for ( int i = 0 ; i < noticeList.size() ; i++ ) {
             int smallestIndex = i;
 
-            for (int j = i; i < noticeList.size(); j++) {
+            for (int j = i; j < noticeList.size(); j++) {
                 if (noticeList.get(smallestIndex).getPostingTime() > noticeList.get(j).getPostingTime()) {
                     smallestIndex = j;
                 }
@@ -168,7 +173,7 @@ public class Sort {
         return noticeList;
     }
 
-    public static ArrayList< Notice > getLendings( ArrayList<Notice> noticeList ){
+    public static ArrayList< Notice > getLendings( ArrayList<Notice> noticeList ){//TESTED AND WORKS
         ArrayList< Notice > sortedList = new ArrayList<Notice>();
 
         for( int i = 0; i < noticeList.size(); i++ ){
@@ -179,7 +184,7 @@ public class Sort {
         return sortedList;
 
     }
-    public static ArrayList< Notice > getBorrowings( ArrayList<Notice> noticeList ){
+    public static ArrayList< Notice > getBorrowings( ArrayList<Notice> noticeList ){//TESTED AND WORKS
         ArrayList< Notice > sortedList = new ArrayList<Notice>();
 
         for( int i = 0; i < noticeList.size(); i++ ){
@@ -189,6 +194,22 @@ public class Sort {
         }
         return sortedList;
 
+    }
+
+    /**
+     * Not tested added recently
+     * @param noticeList
+     * @return
+     */
+    public static ArrayList< Notice > randomize( ArrayList<Notice> noticeList ){
+        for( int i = 0; i < noticeList.size(); i++ ) {
+            int random = (int) (Math.random() * noticeList.size()) + 1;
+            Notice temp = noticeList.get(random);
+            noticeList.set( random, noticeList.get(i) );
+            noticeList.set( i , temp );
+        }
+        return noticeList;
+        
     }
 
 
